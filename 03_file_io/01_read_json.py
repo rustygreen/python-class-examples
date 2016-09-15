ff34c6dcc9e0d14adb8c77a27800025f9997a929
Rusty Green <rustymgreen@gmail.com>
2016-09-15 16:31:16 -0400

    Updates

--- 03_file_io/01_read_json.py
+++ 03_file_io/01_read_json.py
@@ -1,9 +1,11 @@
 import json
+from dateutil.parser import parse
 
-with open(r'assets\people.csv', 'w') as f:
+with open(r'..\assets\people.csv', 'w') as f:
     f.write('one, two, three')
-    with open(r'assets\people.json') as data_file:
+    with open(r'..\assets\people.json') as data_file:
         people = json.load(data_file)
         for person in people:
             f.write(person['firstName'] + '\n')
             print person['firstName']
+            print parse(person['birthday']).day
